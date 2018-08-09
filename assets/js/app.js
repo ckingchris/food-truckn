75e048eba3042fd96fa9c0dbd0d571b40b18a383
ckingchris <ckingwebdesign@gmail.com>
2018-08-08 20:53:07 -0700

    mapbox init, bootstrapped

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2tpbmd3ZWJkZXNpZ24iLCJhIjoiY2prbHpucXhjMDhiczN2dWtwa3p4ZG8xbiJ9.jBb3qjiqTSQI632gAwj-4Q';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 8,
+    center: [-122.431297, 37.773972]
+});
